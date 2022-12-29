d32316da6d81b7d742f5d528e2cac57a7e1a7da3
Stephen Gream <stephen.gream@gmail.com>
2022-12-29 17:22:54 +1100

    Make main take a -file parameter

--- cmd/main.go
+++ cmd/main.go
@@ -1,55 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/ThreeToes/blogvm/internal/assembler"
 	"github.com/ThreeToes/blogvm/internal/machine"
+	"os"
 )
 
-var program = `COPY 0x01 R0
-COPY 0x0A R1
-LOOP COPY 0x01 R2
-MUL R1 R0
-EQ 0x01 R1
-JMP END
-SUB R1 R2
-COPY R2 R1
-COPY 0x01 R2
-JMP LOOP
-END WRITE R0 0x1000
-HALT
-`
-
 func main() {
-	assembled, err := assembler.AssembleString(program)
-	if err != nil {
-		fmt.Printf("could not assemble program: %v", err)
+	if len(os.Args) == 1 {
+		fmt.Println("must provide a command:")
+		fmt.Println("\t* run - run a file")
 		return
 	}
+	switch os.Args[1] {
+	case "run":
+		fs := flag.NewFlagSet("run", flag.ExitOnError)
+		filePath := fs.String("file", "", "path to the file to run")
+		err := fs.Parse(os.Args[2:])
+		if err != nil {
+			fmt.Printf("could not parse args: %v\n", err)
+			return
+		}
+		if *filePath == "" {
+			fmt.Printf("file cannot be empty\n")
+			fs.Usage()
+			return
+		}
+		assembled, err := assembler.AssembleFile(*filePath)
+		if err != nil {
+			fmt.Printf("could not assemble program: %v\n", err)
+			return
+		}
 
-	registers := machine.NewRegisterBank()
-	mem := machine.NewMemory()
-	err = mem.Load(assembled)
-	if err != nil {
-		fmt.Printf("could not load assembled program: %v", err)
-		return
-	}
-	bus := machine.NewBus(mem)
-	cpu := machine.NewCPU(registers, bus)
+		registers := machine.NewRegisterBank()
+		mem := machine.NewMemory()
+		err = mem.Load(assembled)
+		if err != nil {
+			fmt.Printf("could not load assembled program: %v\n", err)
+			return
+		}
+		bus := machine.NewBus(mem)
+		cpu := machine.NewCPU(registers, bus)
 
-	sr, err := registers.GetRegister(machine.SR)
-	if err != nil {
-		fmt.Printf("Could not get the staus register: %v", err)
-		return
+		sr, err := registers.GetRegister(machine.SR)
+		if err != nil {
+			fmt.Printf("Could not get the staus register: %v", err)
+			return
+		}
+		for sr.Value&machine.STATUS_HALT == 0 {
+			cpu.Tick()
+		}
+		fmt.Printf("machine has halted\n")
 	}
-	for sr.Value&machine.STATUS_HALT == 0 {
-		cpu.Tick()
-	}
-	val, err := mem.Read(0x1000)
-	if err != nil {
-		fmt.Printf("could not read memory at 0x1000: %v\n", err)
-		return
-	}
-	// Value in memory should be 3628800
-	fmt.Printf("The value at address 0x1000 is %d\n", val)
 }
